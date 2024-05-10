d11de3f3f424fdb8fcdbfb081b3d824698bf5e2a
levutb2004 <52491685+levutb2004@users.noreply.github.com>
2024-05-10 11:00:08 +0700

    Add files via upload

--- src/ai.py
+++ src/ai.py
@@ -1,6 +1,8 @@
 import random
 from board import Board
 
+CHECKMATE = 1000 
+
 class AI:
     def __init__(self):
         self.square = None
@@ -14,30 +16,38 @@ class AI:
             for square in row: 
                 if square.piece != None and square.piece.color == 'black':
                     self.squares_with_pieces.append(square)
-    
+                    
+    def find_best_move(self, game):
+        score = 0
+        bestMove = None
+        for square in self.squares_with_pieces:
+            self.board.calc_moves(square.piece, square.row, square.col, bool=True)
+            for move in square.piece.moves:
+                if self.move_score(move) > score:
+                    score = self.move_score(move)
+                    bestMove = move
+                    self.piece = square.piece
+        if score > 0: return bestMove
+        else: 
+            while True:
+                square = random.choice(self.squares_with_pieces)
+                if square.piece.moves == []:
+                    continue
+                else:
+                    self.piece = square.piece
+                    move = random.choice(square.piece.moves)
+                    return move
+            
     def move_score(self, move):
         if self.board.squares[move.final.row][move.final.col].has_piece():
             return self.board.squares[move.final.row][move.final.col].piece.value
         return 0
-        
-    def get_best_moves_of_a_piece(self, moves):
-        best_moves = []
-        score = -1
-        for move in moves:
-            if self.move_score(move) > score:
-                score = self.move_score(move)
-        return best_moves
                 
-    def get_greddy_moves(self):
+    def get_random_moves(self):
         while True:
             square = random.choice(self.squares_with_pieces)
             self.board.calc_moves(square.piece, square.row, square.col, bool=True)
-            if square.piece.moves == []:
-                continue
-            else:
-                self.piece = square.piece
-                move = random.choice(square.piece.moves)
-                return move
+            
             
         
         
